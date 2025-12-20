1b16d24e246fc6f74969353d15f7343717d059e5
ChirantanGuin <chirantanguin1@gmail.com>
2025-12-20 20:24:47 +0530

    Added files first upload

---
+++ upload.js
@@ -0,0 +1,4 @@
+const Supabase = supabase.createClient(
+    "https://irpdlkhewvvqjperruxt.supabase.co",   // Replace with your Project URL
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycGRsa2hld3Z2cWpwZXJydXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNTE1NTgsImV4cCI6MjA4MTcyNzU1OH0.9qiUMOAFP_a2V6eKYpTsiG9jbmnW4d5-NDmP-nlni7E"       // Replace with your anon/public key
+  );
